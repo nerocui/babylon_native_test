5102734cf5a30854f582ea4a5fbc578c136841d0
Nero Cui <nerocui@outlook.com>
2023-02-03 02:24:17 -0800

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,57 @@
+/**
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, { useState, FunctionComponent, useEffect } from "react";
+import { SafeAreaView, StatusBar, View, ViewProps } from "react-native";
+import { EngineView, useEngine } from "@babylonjs/react-native";
+import { SceneLoader } from "@babylonjs/core/Loading/sceneLoader";
+import { Camera } from "@babylonjs/core/Cameras/camera";
+import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
+import "@babylonjs/loaders/glTF";
+import { Scene } from "@babylonjs/core/scene";
+
+const EngineScreen: FunctionComponent<ViewProps> = (props: ViewProps) => {
+  const engine = useEngine();
+  const [camera, setCamera] = useState<Camera>();
+  const [scene, setScene] = useState<Scene>();
+
+  useEffect(() => {
+    if (engine) {
+      const url = "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/BoxAnimated/glTF/BoxAnimated.gltf";
+      SceneLoader.LoadAsync(url, undefined, engine).then((scene) => {
+        setScene(scene);
+        scene.createDefaultCameraOrLight(true, undefined, true);
+        (scene.activeCamera as ArcRotateCamera).alpha += Math.PI;
+        (scene.activeCamera as ArcRotateCamera).radius = 10;
+        setCamera(scene.activeCamera!);
+      });
+    }
+  }, [engine]);
+
+  return (
+    <>
+      <View style={props.style}>
+        <View style={{ flex: 1 }}>
+          <EngineView camera={camera} displayFrameRate={true} />
+        </View>
+      </View>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+        <EngineScreen style={{ flex: 1 }} />
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
---
+++ windows/babylon_test/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::babylon_test::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::babylon_test::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
